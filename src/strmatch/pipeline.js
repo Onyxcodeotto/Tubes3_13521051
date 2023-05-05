ed9b4c856019b0a353532d757de5b9e7543ad0af
Onyxcodeotto <110540920+Onyxcodeotto@users.noreply.github.com>
2023-05-05 23:18:30 +0700

    feat: regex

--- src/strmatch/pipeline.js
+++ src/strmatch/pipeline.js
@@ -1,3 +1,26 @@
 class Pipeline{
+    constructor(){
+        this.string = "";
+        this.equationRe = new RegExp();
+        this.dateRe = new RegExp("[\d\+\-\*\/\^\(\)]*");
+        this.addQ = new RegExp("([\w\s]* |)tambah pertanyaan \w[\w\s]* jawaban \w[\w\s]*");
+        this.deleteQ = new RegExp("([\w\s]* |)hapus pertanyaan \w[\w\s]*");
+    }
 
+    determine(input){
+        
+        if(this.dateRe.test()){
+            return this.dateRe.match();
+        }
+        if(this.equationRe.test()){
+            return this.equationRe.match();
+        }
+        if(this.addQ.test()){
+            return this.addQ.match();
+        }
+        if(this.deleteQ.test()){
+            return this.deleteQ.match();
+        }
+    }
 };
+var result = stringMath("2+2");
